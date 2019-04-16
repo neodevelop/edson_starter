ba39f0fac0cc3339571173bf919ddc40dd30d81f
neodevelop <juan@makingdevs.com>
2019-04-16 11:38:04 -0500

    Comienzo con Groovy

---
+++ Hello.groovy
@@ -0,0 +1,14 @@
+public class Hello {
+  private String name;
+  public void setName(String name){ this.name = name; }
+  public String getName(){ return this.name; }
+
+  public String greet() { return "Hello " + this.name +  " !!!"; }
+
+  public static void main(String[] args){
+    Hello object = new Hello();
+    object.setName("Edson");
+    System.out.println(object.greet());
+  }
+
+}
